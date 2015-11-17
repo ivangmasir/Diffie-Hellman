bf6162567c62e25e7c3376b5635a91fe12805535
ivan <ivan@Ivan-MSI>
2015-11-17 15:40:33 +0100

    Implementacion del algoritmo de intercambio de claves Diffie-Hellman en java

--- src/diffie/Diffie.java
+++ src/diffie/Diffie.java
@@ -9,19 +9,20 @@ class Diffie
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         
         
-        System.out.println("Alice, elige un numero primo (p): ");
+        System.out.println("Alice, elige un numero primo (p) (Ejem. 23): ");
         BigInteger p=new BigInteger(br.readLine());
         
-        System.out.print("Alice, elige la base(g) menor que: "+p+" : ");
+        System.out.print("Alice, elige la base(g) menor que: "+p+" (Ejem. 5): ");
         BigInteger g=new BigInteger(br.readLine());
         
-        System.out.println("Alice, elige un numero secreto, menor que "+p+":");
+        System.out.println("Alice, elige un numero secreto, menor que "+p+" (Ejem. 6):");
         BigInteger x=new BigInteger(br.readLine());
+        
         BigInteger R1=g.modPow(x,p);
         
         System.out.println("R1="+R1);
         
-        System.out.print("Bob, elige un numero secreto menor que "+p+":");
+        System.out.print("Bob, elige un numero secreto menor que "+p+": (Ejem. 15)");
         BigInteger y=new BigInteger(br.readLine());
        
         BigInteger R2=g.modPow(y,p);
