a6eb4c890b9d8775dd869cb4f23ae3355164acbd
ivan <ivan@Ivan-MSI>
2015-11-23 19:10:10 +0100

    Añadida comprobacion de si el numero es primo y si los numeros introducidos estan dentro dle rango esperado

--- src/diffie/Diffie.java
+++ src/diffie/Diffie.java
@@ -12,19 +12,40 @@ class Diffie
         System.out.println("Alice, elige un numero primo (p) (Ejem. 23): ");
         BigInteger p=new BigInteger(br.readLine());
         
-        System.out.print("Alice, elige la base(g) menor que: "+p+" (Ejem. 5): ");
+        while (p.isProbablePrime(1)==false){
+            System.out.println("El numero "+p+" no es primo: ");
+            System.out.println("Alice, elige un numero primo (p) (Ejem. 23): ");
+            p=new BigInteger(br.readLine());
+        }
+        
+        System.out.println("Alice, elige la base(g) menor que: "+p+" (Ejem. 5):");
         BigInteger g=new BigInteger(br.readLine());
+        while (g.compareTo(p)>=0){
+          System.out.println("\nEl numero "+g+" no es menor que "+p);
+          System.out.print("Alice, elige la base(g) menor que: "+p+" (Ejem. 5): ");
+          g=new BigInteger(br.readLine());
+        }
         
         System.out.println("Alice, elige un numero secreto, menor que "+p+" (Ejem. 6):");
         BigInteger x=new BigInteger(br.readLine());
+        while(x.compareTo(p)>=0){
+            System.out.println("El numero "+x+" no es menor que " +p);
+            System.out.println("Alice, elige un numero secreto, menor que "+p+" (Ejem. 6):");
+            x=new BigInteger(br.readLine());
+        }
         
         //p^x mod g
         BigInteger R1=g.modPow(x,p);
         
         System.out.println("R1="+R1);
         
-        System.out.print("Bob, elige un numero secreto menor que "+p+": (Ejem. 15)");
+        System.out.println("Bob, elige un numero secreto menor que "+p+": (Ejem. 15)");
         BigInteger y=new BigInteger(br.readLine());
+        while(y.compareTo(p)>=0){
+            System.out.println("El numero "+y+" no es menor que "+p);
+            System.out.println("Bob, elige un numero secreto menor que "+p+": (Ejem. 15)");
+            y=new BigInteger(br.readLine());
+        }
        //p^y mod g
         BigInteger R2=g.modPow(y,p);
         
